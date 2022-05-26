60d44c18b083dd3e5edb8580ca62381f59d18f2e
Channing Norton <cnorton@pc-solutions.it>
2022-05-26 15:33:51 -0400

    moved over to es6 module, addresses the export issue. Settings menu still not appearing

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,15 @@
-console.log('foundry-accessibility-toolkit | Hello World!');
-export const registerSettings = () =>{
-    game.settings.registerMenu("foundry-accessibility-toolkit", "Target Indicator Size", {
-        name: "Targeting Indicator Size",
-        hint: "Adjusts the size of the dot applied to tokens when OTHER players target them",
-        scope: "client",
-        config: true,
-        type: Number,
-        range: {
-            min: 10,
-            max: 100,
-            step: 10
-        },
-        default: 20,
-        onChange: value => console.log(value)
-    });
-
-};
+console.log("Foundry Accessibility Toolkit Is indeed loading main.js")
+game.settings.register("foundry-accessibility-toolkit", "Target Indicator Size", {
+    name: "Targeting Indicator Size",
+    hint: "Adjusts the size of the dot applied to tokens when OTHER players target them",
+    scope: "client",
+    config: true,
+    type: Number,
+    range: {
+        min: 10,
+        max: 100,
+        step: 10
+    },
+    default: 20,
+    onChange: value => console.log(value)
+});
