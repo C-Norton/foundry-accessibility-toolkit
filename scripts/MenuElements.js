ed9474182fd6863795ba1ab29445bb62b8507272
Channing Norton <cnorton@pc-solutions.it>
2022-05-30 19:40:45 -0400

    Working on debugging this strange typeerror

---
+++ scripts/MenuElements.js
@@ -0,0 +1,1 @@
+class MenuElements extends FormApplication{}
--- scripts/settings.js
+++ scripts/settings.js
@@ -12,7 +12,22 @@ const settingblock = {
         step: 10
     },
     default: 20,
-    onChange: value => console.log(value)
+    onChange: value => {console.log(value)}
 };
 game.settings = new ClientSettings();
-game.settings.registerMenu(moduleName, settingsTargetSize, settingblock);
+game.settings.registerMenu("foundryaccessibilitytoolkit", "targetsize", {
+    name: "Register a Module Setting with a Range slider",
+    hint: "A description of the registered setting and its behavior.",
+    scope: "world",      // This specifies a world-level setting
+    config: true,        // This specifies that the setting appears in the configuration view
+    type: Number,
+    range: {             // If range is specified, the resulting setting will be a range slider
+        min: 0,
+        max: 100,
+        step: 10
+    },
+    default: 50,         // The default value for the setting
+    onChange: value => { // A callback function which triggers when the setting is changed
+        console.log(value)
+    }
+});
